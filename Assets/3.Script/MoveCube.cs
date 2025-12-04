7a0289504a6e422f275653a3e132ada6e58173e8
SolleafTea <spoo05100@gmail.com>
2025-12-04 12:34:07 +0900

    CubeRotate

---
+++ Assets/3.Script/MoveCube.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+
+
+public class MoveCube : MonoBehaviour
+{ 
+    void Update()
+    {
+        transform.Rotate(new Vector3(10f, 20f, 30f) * Time.deltaTime);    
+    }
+}
